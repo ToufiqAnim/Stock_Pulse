0c6ccf7476db33d0fff5abe4ee413276c75f19b1
Toufiqul Islam Anim <toufiq9610@gmail.com>
2025-12-10 06:31:57 +0600

    NavItems.tsx and userDropdown created

--- components/UserDropdown.tsx
+++ components/UserDropdown.tsx
@@ -1,5 +1,48 @@
+"use client";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "./ui/button";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { useRouter } from "next/navigation";
 const UserDropdown = () => {
-  return <div>UserDropdown</div>;
+  const router = useRouter();
+  const handleSignOut = async () => {
+    router.push("/sign-in");
+  };
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant="ghost"
+          className="flex items-center gap-3 text-gray-4 hover:text-yellow-500"
+        >
+          <Avatar className="h-8 w-8">
+            <AvatarImage src="https://i.ibb.co.com/fd95b1Bx/user-profile-icon-circle-1256048-12499.avif" />
+            <AvatarFallback className="bg-yellow-500 text-yellow-900 text-sm font-bold">
+              Toufiq
+            </AvatarFallback>
+          </Avatar>
+          <div className="hidden md:flex flex-col items-start">
+            <span className="text-base font-medium text-gray-400">Toufiq</span>
+          </div>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>Profile</DropdownMenuItem>
+        <DropdownMenuItem>Billing</DropdownMenuItem>
+        <DropdownMenuItem>Team</DropdownMenuItem>
+        <DropdownMenuItem>Subscription</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
 };
 
 export default UserDropdown;
