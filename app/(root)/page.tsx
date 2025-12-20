057f63569db1389f80625b87653da446bf567d69
Toufiqul Islam Anim <toufiq9610@gmail.com>
2025-12-20 16:15:09 +0600

    file structure created and necessary files downloaded

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+const Home = () => {
+  return (
+    <div className="flex min-h-screen home-wrapper">
+      <Button> Home </Button>
+    </div>
+  );
+};
+
+export default Home;
